866ef3af2e764bd8ef512fa757ea0a1f036eb7af
Codejuma <jumaongoro5@gmail.com>
2023-06-25 08:17:01 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -10,15 +10,15 @@ int _printf(const char *format, ...)
 	int count;
 	va_list args;
 
-	if (*format == NULL)
+	if (format == NULL)
 		return (-1);
 
-	count = _strlen(*format);
+	count = _strlen(format);
 	if (count <= 0)
 		return (0);
 
-	va_start(args, *format);
-	count = handler(*format, args);
+	va_start(args, format);
+	count = handler(format, args);
 
 	_putchar(-1);
 	va_end(args);
