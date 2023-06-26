63019999dea6863dc766d73f8897297a277b73d6
Codejuma <jumaongoro5@gmail.com>
2023-06-26 16:22:10 -0700

    handler.c

--- handler.c
+++ handler.c
@@ -30,6 +30,84 @@ int handler(const char *s, va_list num)
 	return (count);
 }
 
+#include <stdarg.h>
+#include <stdio.h>
+
+typedef struct {
+	char type;
+	int (*f)(va_list);
+} format;
+
+int print_string(va_list num) {
+	char* str = va_arg(num, char*);
+	printf("%s", str);
+	return 1;
+}
+
+int print_char(va_list num) {
+	char* str = va_arg(num, int);
+	printf("%c",c);
+	return 1;
+}
+
+int print_integer(va_list num) {
+	int n = va_arg(num, int);
+	printf("%d", n);
+	return 1;
+}
+int print_unsigned(va_list num) {
+	unsigned int n = va_arg(num, unsigned int);
+	printf("%u", n);
+	return 1;
+}
+
+int print_binary(va_list num) {
+	int n = va_arg(num, int);
+	printf("%d", n);
+	return 1;
+}
+
+int print_unsigned(va_list num) {
+	unsigned int n = va_arg(num, unsigned int);
+	printf("%u", n);
+	return 1;
+}
+
+int print_octal(va_list num) {
+	int n = va_arg(num, int);
+	printf("%o", n);
+	return 1;
+}
+
+int print_hexadecimal_low(va_list num) {
+	int n = va_arg(num, int);
+	printf("%x", n);
+	return 1;
+}
+
+int print_hexadecimal_upp(va_list num) {
+	int n = va_arg(num, int);
+	printf("%X", n);
+	return 1;
+}
+
+int print_rev_string(va_list num) {
+	char* str = va_arg(num, char*);
+	printf("%s", str);
+	return 1;
+}
+
+int print_rot(va_list num) {
+	char* str = va_arg(num, char*);
+	printf("%s", str);
+	return 1;
+}
+
+int _putchar(char c) {
+	putchar(c);
+	return 1;
+}
+
 /**
  * percent_handler - percent format controller
  * @s: string format
@@ -47,7 +125,7 @@ int percent_handler(const char *s, va_list num, int *i)
 		{'b', print_binary}, {'u', print_unsigned},
 		{'o', print_octal}, {'x', print_hexadecimal_low},
 		{'X', print_hexadecimal_upp}, {'p', print_pointer},
-		{'r', print_rev_string}, {'R', print_octal}
+		{'r', print_rev_string}, {'R', print_rot}
 	};
 	*i = *i + 1;
 	if (s[*i] == '\0')
